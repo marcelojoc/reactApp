900a037f897aa28762dbe9092a7a68d73d41edbd
marcelojoc <marcelojoc@gmail.com>
2022-03-08 14:03:12 -0300

    uso  de useState hook

--- react-init/src/components/TodoSearch.jsx
+++ react-init/src/components/TodoSearch.jsx
@@ -1,16 +1,21 @@
-import React from 'react';
+import React , {useState} from 'react';
 import './../css/TodoSearch.css'
 function TodoSearch() {
+
+  const [searchValue, setSearchValue] = useState(''); // llamo al UseState  de react
 
   const onSearchValueChanges = (event) =>{
 
     console.log(event.target.value);
+    setSearchValue(event.target.value); // modifico el State  con los valores
+
   }
 
   return (
     <div>
-      <input placeholder="prueba de placeholder" className='TodoSearch' onChange={onSearchValueChanges}></input>
+      <input placeholder="prueba de placeholder" className='TodoSearch' value={searchValue} onChange={onSearchValueChanges}></input>
       
+      <p>{searchValue}</p>
     </div>
   );
 }
